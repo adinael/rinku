7c216300d05970146930a85acb5f4e46c7910a88
Adinael Perez Ruelas <Adin692@gmail.com>
2019-02-24 22:32:22 -0700

    se agrega procedimiento proc_consultarsiguientenumeroempleado

---
+++ scripts/proc_consultarsiguientenumeroempleado.sql
@@ -0,0 +1,11 @@
+IF EXISTS( SELECT * from sys.objects where name = 'proc_consultarsiguientenumeroempleado' and type = 'P' ) 
+BEGIN 
+	DROP PROC dbo.proc_consultarsiguientenumeroempleado
+END
+GO
+CREATE PROCEDURE dbo.proc_consultarsiguientenumeroempleado 
+AS 
+BEGIN 
+	SELECT ISNULL(IDENT_CURRENT ('dbo.empleados'),90) + 10 AS numeroempleado;  
+END
+GO 
